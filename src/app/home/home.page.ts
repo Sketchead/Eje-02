5ccf2f48b947e616f5ad4acad4dd27069026a632
Sketchead <megamam_atxel@hotmail.com>
2022-10-13 21:28:18 -0600

    Cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public photos = [];
 
-  constructor() {}
+  constructor() {
+    this.photos = [{
+      name: "Foto 1",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae deserunt",
+      url: "https://picsum.photos/id/237/200/300",
+    },
+    {
+      name: "Foto 2",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae deserunt",
+      url: "https://picsum.photos/id/352/200/300",
+    },
+    {
+      name: "lemusiano",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae deserunt",
+      url: "https://picsum.photos/id/242/200/300",
+    }
+  ]
+  }
 
 }
